80b43a4335680d34766c8c2715081720ed6c3561
Mullins, Benjamin <benm@umich.edu>
2022-02-01 20:17:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [timerAmount, setTimerAmount] = useState(0);
+  const [buttonClicked, setButtonClicked] = useState('default');
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(()=> {
+    if (buttonClicked === 'start') {
+      const interval = setInterval(() => {
+        setTimerAmount(previousAmount => previousAmount + 1)
+      }, 1000);
+      setIntervalId(interval);
+    }  else {
+      clearInterval(intervalId);
+      if (buttonClicked === 'reset') {
+        setTimerAmount(0);
+      }
+    }
+  }, [buttonClicked]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">
+        <strong>TIME:</strong>
+        <output>{timerAmount}</output>
+      </div>
+      <div className="btn-group">
+          <button 
+            onClick={() => {
+              setButtonClicked('start')}}
+            disabled={buttonClicked === 'start'}>
+            Start
+          </button>
+          <button  
+            onClick={() => setButtonClicked('stop')}
+            disabled={['default', 'stop', 'reset'].includes(buttonClicked)}>
+              Stop
+          </button>
+          <button  
+            onClick={() => setButtonClicked('reset')}
+            disabled={['default', 'reset'].includes(buttonClicked)}>
+              Reset
+            </button>
+      </div>
     </div>
   );
 }
